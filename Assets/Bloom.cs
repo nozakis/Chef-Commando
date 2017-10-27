3161955ae72cf42f09494d51c510eed32b56592b
Martin <marth.hehe@gmail.com>
2017-10-27 11:40:09 +0200

    You can throw shit

---
+++ Assets/Bloom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bloom : MonoBehaviour {
+    [SerializeField] Image largeReticule;
+
+    void Update() {
+        RaycastHit hitInfo;
+        // Raycast from the center of the screen two units forward
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 2)
+                && hitInfo.transform.GetComponentInParent<Pickup>() != null
+                && hitInfo.transform.GetComponentInParent<Pickup>().pickupsRemaining > 0) {
+            largeReticule.gameObject.SetActive(true);
+        } else {
+            largeReticule.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/GrabThrow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabThrow : MonoBehaviour {
+    [SerializeField] Transform pickupSpawn;
+
+    private GameObject heldObject;
+    private bool holdingItem = false;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            if (!holdingItem) {
+                RaycastHit hitInfo;
+                // Raycast from the center of the screen two units forward
+                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 2)) {
+                    Pickup pickup = hitInfo.transform.GetComponentInParent<Pickup>();
+                    if (pickup != null) {
+                        GameObject pickupObj = pickup.GrabPickup();
+                        if (pickupObj != null) {
+                            heldObject = GameObject.Instantiate(pickupObj, pickupSpawn);
+                            holdingItem = true;
+                        }
+                    }
+                }
+            } else {
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Collider>().enabled = true;
+                Rigidbody heldRB = heldObject.GetComponent<Rigidbody>();
+                heldRB.isKinematic = false;
+                Vector3 forward = heldRB.transform.forward;
+                Vector3 throwDir = new Vector3(forward.x, forward.y + .4f, forward.z);
+                heldRB.AddForce(throwDir * 500);
+                holdingItem = false;
+            }
+        }
+    }
+}
